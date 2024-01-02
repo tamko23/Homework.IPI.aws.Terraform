e69b47e42be8456054ee196801bbbdfc6c058674
Tamba KOMANO <124822702+tamko23@users.noreply.github.com>
2024-01-03 00:14:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,24 @@
+# Variable pour spécifier la clé d'accès AWS
 variable "access_key" {
   description = "Access key to AWS console"
 }
+# Variable pour spécifier la clé secrte AWS
 variable "secret_key" {
   description = "Secret key to AWS console"
 }
 
-
-variable "instance_name" {
-  description = "Name of the instance to be created"
-  default     = "awsbuilder-demo"
-}
-
+# Variable pour spécifier le type d'instance EC2.
 variable "instance_type" {
   default = "t2.micro"
 }
 
-
+# Variable pour spécifier le type EC2 à lancer.
 variable "ami_id" {
   description = "The AMI to use"
   default     = "ami-09d56f8956ab235b3"
 }
 
+# Variable pour spécifier le bloc CIDR pour le VPC.
 variable "vpc-cidr" {
   default = "10.10.0.0/16"
 }
